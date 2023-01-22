3d233c1a89ae7e25ec90e95def0825621504a46c
brennerpaiva <114958953+brennerpaiva@users.noreply.github.com>
2023-01-22 14:30:30 -0300

    correção js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,19 @@
-var modal = document.querySelector(".modal")
+const botaoTrailer = document.querySelector(".botao-trailer");
+const botaoFecharModal = document.querySelector(".fechar-modal");
+const video = document.getElementById("video");
+const modal = document.querySelector(".modal");
+const linkDoVideo = video.src;
 
-function mostrarModal() {
-    modal.classList.add("aberto")
+function alternarModal() {
+    modal.classList.toggle("aberto");   //toggle(alternar)
 }
 
-function fecharModal() {
-    modal.classList.remove("aberto")
-}
+botaoTrailer.addEventListener("click", () => {
+    alternarModal();
+    video.setAttribute("src", linkDoVideo);
+})
+
+botaoFecharModal.addEventListener("click", () => {
+    modal.classList.toggle("aberto");
+    video.setAttribute("src", "");
+})
